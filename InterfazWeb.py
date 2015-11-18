564a578d744040846fa96f9c2bd26b0d08b02924
Diego Castaño Chillarón <diego.castano@alumnos.upm.es>
2015-11-18 13:58:28 +0100

    Fin interfaz

--- InterfazWeb.py
+++ InterfazWeb.py
@@ -1,21 +1,64 @@
+# -*- coding: utf-8 -*- #
 import logging
+import hashlib
+import json
+from pprint import pprint
 from flask import Flask, request, render_template, send_from_directory
 from os import listdir
+import os
 from os.path import isfile, join
 
-# Ruta relativa al directio de tareas Luigi
+# Puerto publico del servidor
+PORT = 8081
+
+# Hash de la contraseña de acceso
+PASSWORD_SHA1_HASH = '';
+
+# Ruta relativa al directorio de tareas Luigi
 TASKS_PATH = '../'
 
 app = Flask(__name__)
-app.debug = True
 
-@app.route('/tasks/<nombre>')
+@app.route('/tareas/<nombre>')
 def returnTaskCode(nombre):
     return send_from_directory(TASKS_PATH, nombre)
+
+@app.route('/css/<nombre>')
+def returnStyle(nombre):
+    return send_from_directory('templates/css', nombre)
 
 @app.route('/js/<nombre>')
 def returnScript(nombre):
     return send_from_directory('templates/js', nombre)
+
+@app.route('/acceder')
+def acceder():
+    password = request.args.get('password', '')
+    passwordHash = hashlib.sha1(password).hexdigest()
+    if passwordHash == PASSWORD_SHA1_HASH:
+        return 'ok'
+    else:
+        return 'error'
+
+@app.route('/lanzar/<categoria>/<tarea>', methods=['POST'])
+def lanzarTarea(categoria, tarea):
+    llamada = 'PYTHONPATH=\'\' luigi --module ' + categoria + ' ' + tarea + ' --local-scheduler'
+    jsonObject = request.get_json()
+    password = jsonObject['password']
+    parametros = jsonObject['parametros[]']
+
+    for parametro in parametros:
+        llamada += ' --' + parametro['nombre'] + ' ' + parametro['valor']
+
+    passwordHash = hashlib.sha1(password).hexdigest()
+    if passwordHash == PASSWORD_SHA1_HASH:
+        try:
+            # os.system(llamada)
+            return 'ok'
+        except e:
+            return e
+    else:
+        return 'error'
 
 @app.route('/')
 def index():
@@ -29,4 +72,4 @@ def index():
     return render_template('index.html', categorias=categorias)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=PORT)
--- templates/js/index.js
+++ templates/js/index.js
@@ -1,38 +1,161 @@
-/*global $, console*/
-(function ($) {
+/*global $, console, localStorage*/
+(function ($, localStorage) {
     'use strict';
+
+    var tareasEndpoint = '/tareas/',
+        lanzarEndpoint = '/lanzar/',
+        password = '';
+
+    function acceder() {
+        password = localStorage.getItem('password') || $('#password').val();
+        $.get('/acceder', { password: password})
+            .done(function (data) {
+                if (data === 'ok') {
+                    localStorage.setItem('password', password);
+                    $('#panel').toggle();
+                    $('#acceso').toggle();
+                }
+            });
+    }
+
+    function lanzarTarea() {
+        var categoria = $('#categoria option:selected').text(),
+            tarea = $('#tarea').val(),
+            parametros = [],
+            url = lanzarEndpoint + categoria + '/' + tarea,
+            inputsParametro = $('.parametro'),
+            data;
+
+        inputsParametro.each(function () {
+            parametros.push({
+                nombre: $(this).attr('name'),
+                valor: $(this).val()
+            });
+        });
+
+        data = {
+            'password': password,
+            'parametros[]': parametros
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: JSON.stringify(data),
+            contentType: 'application/json; charset=utf-8'
+        }).done(function (data) {
+            $('#panel').toggle();
+            if (data === 'ok') {
+                $('#success').toggle();
+            } else {
+                $('#error').toggle();
+            }
+        });
+    }
 
     function getTareas(contenido) {
         var tareas = [],
             lineas = contenido.split(/\r?\n/);
+
         lineas.forEach(function (linea) {
             if (linea.indexOf('class ') > -1) {
-                var tarea = linea.split(" ")[1].split("(")[0];
+                var tarea = linea.split(' ')[1].split('(')[0];
                 tareas.push(tarea);
             }
         });
         return tareas;
     }
 
-    function updateTasks() {
-        var tasksEndpoint = '/tasks/',
-            fichero = $('#categoria').val();
+    function getComentario(contenido, nombreTarea) {
+        var clase = contenido.split('class ' + nombreTarea)[1].split('class ')[0],
+            comentario = clase.split('\"\"\"')[1] || 'Esta tarea no tiene descripción.';
+        return comentario;
+    }
 
-        $.get(tasksEndpoint + fichero)
+    function getParametros(contenido, nombreTarea) {
+        var parametros = [],
+            lineasClase = contenido.split('class ' + nombreTarea)[1].split('class ')[0].split(/\r?\n/);
+
+        lineasClase.forEach(function (lineaClase) {
+            if (lineaClase.indexOf('luigi.Parameter(') > -1) {
+                var nombreParametro = lineaClase.split(' = ')[0],
+                    defaultValue = '';
+                if (lineaClase.indexOf('luigi.Parameter(default="') > -1) {
+                    defaultValue = lineaClase.split('luigi.Parameter(default="')[1].split('")')[0];
+                }
+                parametros.push({
+                    'nombre': nombreParametro.replace('\t', ''),
+                    'valor': defaultValue.replace('\t', '')
+                });
+            }
+        });
+        return parametros;
+    }
+
+    function updateComentario() {
+        var fichero = $('#categoria').val(),
+            tarea = $('#tarea').val();
+
+        $.get(tareasEndpoint + fichero)
+            .done(function (data) {
+                var info = getComentario(data, tarea);
+                $('#comentario').empty();
+                $('#comentario').html(info);
+            });
+    }
+
+    function updateParametros() {
+        var fichero = $('#categoria').val(),
+            tarea = $('#tarea').val();
+
+        $.get(tareasEndpoint + fichero)
+            .done(function (data) {
+                var htmlParametro = '',
+                    parametros = getParametros(data, tarea);
+                $('#parametros').empty();
+                if (parametros.length < 1) {
+                    $('#parametros').append('<span>Esta tarea no admite parámetros</span>');
+                    return;
+                }
+                parametros.forEach(function (parametro) {
+                    htmlParametro = '<label>' + parametro.nombre
+                                        + ': </label><input class="parametro form-control" name="'
+                                        + parametro.nombre + '" type="text" value="'
+                                        + parametro.valor + '"/><br/>';
+                    $('#parametros').append(htmlParametro);
+                });
+            });
+    }
+
+
+
+    function updateTareas() {
+        var fichero = $('#categoria').val();
+
+        $.get(tareasEndpoint + fichero)
             .done(function (data) {
                 var tareas = getTareas(data);
                 $('#tarea').empty();
                 tareas.forEach(function (tarea) {
                     $('#tarea').append('<option value="' + tarea + '">' + tarea + '</option>');
                 });
+                updateParametros();
+                updateComentario();
             });
     }
 
     function onLoad() {
-        updateTasks();
-        $('#categoria').change(updateTasks);
+        $('#panel').toggle();
+        $('#success').toggle();
+        $('#error').toggle();
+        acceder();
+        updateTareas();
+        $('#categoria').change(updateTareas);
+        $('#tarea').change(updateParametros);
+        $('#acceder').click(acceder);
+        $('#lanzar').click(lanzarTarea);
     }
 
     $(onLoad());
 
-}($));
+}($, localStorage));
